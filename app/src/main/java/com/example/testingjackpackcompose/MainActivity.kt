f0bf1ffb9f540928b398bb0ce9c7c47198157354
magerngulik <magerngulik@gmail.com>
2023-09-18 15:03:47 +0700

    belajar android jetpack compose

---
+++ app/src/main/java/com/example/testingjackpackcompose/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.testingjackpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.testingjackpackcompose.ui.theme.TestingJackpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
